3f92c261f800f9a09192bd84d1ba7de62a259032
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-06 15:00:38 -0300

    Add style

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { increment, decrement } from './counterSlice'
+import styles from './Counter.module.css'
 
 export const Counter = () => {
   const count = useSelector(state => state.counter.value)
@@ -8,15 +9,19 @@ export const Counter = () => {
 
   return (
     <div>
-      <div>
+      <div className={styles.row}>
         <button
+          className={styles.button}
           aria-label='Increment value'
           onClick={() => dispatch(increment())}
         >
           Increment
         </button>
-        <span>{count}</span>
+
+        <span className={styles.value}>{count}</span>
+
         <button
+          className={styles.button}
           aria-label='Decrement value'
           onClick={() => dispatch(decrement())}
         >
