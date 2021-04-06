b0ac21be14c0283f41db9fad8bb414979cda1d56
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-06 14:55:59 -0300

    Use Redux State and Actions in React Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import { Counter } from './features/counter/Counter'
 
 function App() {
-  return <div className='App'>
-    teste
-  </div>
+  return (
+    <div className='App'>
+      <Counter />
+    </div>
+  )
 }
 
 export default App
